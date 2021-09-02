08260240985b40520470779f8c5b4b092fbef539
Fabio <fabio.ciampichetti@gmail.com>
2021-09-02 16:41:36 +0200

    aggiunta file

---
+++ javascript/script.js
@@ -0,0 +1,47 @@
+
+const onClick = document.querySelector('.click');
+const display = document.querySelector('.number');
+
+// creo il bottone "+"
+let btnPlus = document.createElement('button');
+btnPlus.textContent = '+';
+onClick.appendChild(btnPlus);
+btnPlus.setAttribute('class', 'styles');
+
+// creo il bottone "-"
+let btnLess = document.createElement('button');
+btnLess.textContent = '-';
+onClick.appendChild(btnLess);
+btnLess.setAttribute('class', 'styles');
+
+// creo il contatore
+let viewNumber = document.createElement('p'); 
+viewNumber.textContent = '0';
+display.appendChild(viewNumber);
+viewNumber.setAttribute('class', 'number');
+
+
+// passaggio per creare l'incremento e il decremento del counter
+const counterDisplayElem = document.querySelector('.number');
+
+let counter = 0;
+
+btnPlus.addEventListener("click",()=> {
+    counter++;
+    updateDisplay();
+});
+
+btnLess.addEventListener("click",()=> {
+    if(counter != 0) {
+        counter--;
+    }
+    updateDisplay();
+});
+
+function updateDisplay() {
+    counterDisplayElem.innerHTML = counter;
+};
+
+updateDisplay();
+
+
